6944acf6ec81e0f05e2260acb5105a7042bcf236
WRTRTYYIFOPU <123384890+WRTRTYYIFOPU@users.noreply.github.com>
2023-03-15 09:47:54 +0300

    Add files via upload

--- YukkiMusic/plugins/play/medo.py
+++ YukkiMusic/plugins/play/medo.py
@@ -14,9 +14,40 @@ from strings import get_command
 
 disable_cut = []
 
+@app.on_message(filters.regex("^تعطيل ردود البـوت$") & filters.group)
+async def descut(client, message):
+      a = await app.get_chat_member(message.chat.id, message.from_user.id)
+      id = message.from_user.id
+      cid = message.chat.id
+      if cid in disable_cut:
+        return await message.reply_text("")
 
+      if not a.can_manage_chat:
+         await message.reply_text("**- هذا الأمر يخص المشرفين بس !**")
+
+          
+      if a.can_manage_chat:
+        disable_cut.append(cid)
+        await message.reply_text(f"- بواسطة {message.from_user.mention}\n- تم تعطيل صراحه")
         
-@app.on_message(filters.regex("^صراحه$") & filters.group)
+@app.on_message(filters.regex("^تفعيل ردود البـوت$") & filters.group)
+async def enacut(client, message):
+      a = await app.get_chat_member(message.chat.id, message.from_user.id)
+      id = message.from_user.id
+      cid = message.chat.id
+      if cid not in disable_cut:
+        return await message.reply_text("")
+        
+
+      if not a.can_manage_chat:
+         await message.reply_text("**- هذا الأمر يخص المشرفين بس !**")
+      
+      if a.can_manage_chat:
+        disable_cut.remove(cid)
+        await message.reply_text(f"- بواسطة {message.from_user.mention}\n- تم تفعيل صراحه")        
+        
+        
+@app.on_message(filters.regex("^بوت$") & filters.group)
 def searchMusic(c: Client, m: Message):
         ch = m.chat.id
         if ch in disable_cut:
@@ -35,4 +66,4 @@ def searchMusic(c: Client, m: Message):
 "**أمـرني وش اشغل لك**",
         ]
         word = random.choice(txt)
-        m.reply_text
+        m.reply_text(f"")
