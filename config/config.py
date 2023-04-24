c67b8ed7380b8d7fd31a469392e2f9f813a2d124
aabb124 <127970495+aabb124@users.noreply.github.com>
2023-04-24 21:51:20 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -30,7 +30,7 @@ SONG_DOWNLOAD_DURATION = int(
 ) 
 
 # جروب اللي مرفوع فيه البوت والمساعد ادمن 
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""-1001645162547))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001645162547"))
 
 # اسم البوت مينفعش تكتبو مزغرف
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME","moon")
